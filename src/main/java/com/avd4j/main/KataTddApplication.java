45b7c823a8ab3bca46fe5804d324f4fbc4b453cf
Anthony <anthony-externe.viard@edf.fr>
2018-06-11 23:39:44 +0200

    first commit'

---
+++ src/main/java/com/avd4j/main/KataTddApplication.java
@@ -0,0 +1,12 @@
+package com.avd4j.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataTddApplication.class, args);
+	}
+}
---
+++ src/test/java/com/avd4j/main/KataTddApplicationTests.java
@@ -0,0 +1,16 @@
+package com.avd4j.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KataTddApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
